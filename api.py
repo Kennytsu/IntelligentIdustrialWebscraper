1e0b6ed63526057b3042964b1282a8ff872ce46f
ge95bid <ge95bid@tum.de>
2024-08-10 22:50:56 +0200

    First steps to gaining the api from alibaba

--- api.py
+++ api.py
@@ -2,10 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import os
+from pprint import pprint
 
+debug = 0
 
 # Standard Amazon Webpage
-url = 'https://www.amazon.com'
+url = 'https://german.alibaba.com/trade/search?spm=a2700.product_home_newuser.home_new_user_first_screen_fy23_pc_search_bar.associationItem_pos_0&tab=all&SearchText=elektro+scooter'
 
 # Simple header for request (Needs to be fixed to excape bot detection)
 headers = {
@@ -20,9 +22,22 @@ proxies = {
 # Sending request to webpage
 response = requests.get(url, proxies=proxies)
 
-# Standard process to improve the response from the request
-soup = BeautifulSoup(response.content, 'html.parser')
+if response.status_code == 200:
+  print("Successful connection \n")
 
-# Prints the result
-print(soup)
+  soup = BeautifulSoup(response.text, 'html.parser')
+    
+  # Find the span element with the class 'search-card-e-price__original'
+  price_elements = soup.find_all('span', class_='search-card-e-price')
+  print(price_elements)
+  
+  for idx, price_element in enumerate(price_elements):
+    price = price_element.text.strip().replace('\xa0', ' ')
+    print(f"Price {idx + 1}: {price}")
+elif ~debug:
+  print(f"Failed to retrieve data. Status code: {response.status_code}")
+else:
+  # Prints the result
+  soup = BeautifulSoup(response.content,'html.parser')
+  print(soup)
 
